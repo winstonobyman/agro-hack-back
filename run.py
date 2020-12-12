e74c1dfd9c5e3114cc74e49b3a6d9514c2001dd5
Ivan <zaykin.ivan.and@gmail.com>
2020-12-12 21:50:08 +0300

    Plot API done

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
-
-from typing import Optional
+import os
+# from typing import Optional
+from utils import get_weekly_field, get_sensor_data, get_dates_from_series
 
 app = FastAPI()
 
-@app.get('/')
-def get_root():
-    return {'Hello': 'World'}
+SENSOR_PATH = os.path.join('data', 'sensor_data.csv')
+SENSOR_DATA = get_sensor_data(SENSOR_PATH)
+
+@app.get('/greenhouse/{grh_num}')
+def get_greenhouse_plot_data(grh_num=1):
+    air_temperatures = get_weekly_field(SENSOR_DATA, 'air_temperature')
+    return {
+        'temperatures': list(air_temperatures.values),
+        'lightningLevels': list(get_weekly_field(SENSOR_DATA, 'illumination_p' + str(grh_num)).values),
+        'soilMoisture': list(get_weekly_field(SENSOR_DATA, 'relative_soil_mosture_p' + str(grh_num)).values),
+        'soilAcidity': list(get_weekly_field(SENSOR_DATA, 'relative_soil_mosture_p' + str(grh_num)).values),
+        'date': list(get_dates_from_series(air_temperatures))
+    }
---
+++ utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+def get_sensor_data(path):
+    sensor_data = pd.read_csv(path).drop(
+        ['Unnamed: 0'], axis=1).set_index('date')
+    sensor_data.index = pd.to_datetime(sensor_data.index)
+    sensor_data = sensor_data.sort_index(ascending=False)
+    return sensor_data
+
+def get_weekly_field(df, field, amount=30):
+  return df[field].resample('W').mean().fillna('')[:amount]
+
+def get_dates_from_series(series):
+    return list(map(lambda x: str(x)[:10], list(series.index)))
