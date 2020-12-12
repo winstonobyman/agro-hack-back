ea171af9fe1a2a7ff285b90af4465e27855e6ee5
Kate <kleo.good@gmail.com>
2020-12-13 00:22:47 +0300

    d

--- run.py
+++ run.py
@@ -3,11 +3,20 @@ import os
 # from typing import Optional
 from utils import get_weekly_field, get_sensor_data, get_dates_from_series
 from ml.conditions import usage
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 SENSOR_PATH = os.path.join('data', 'sensor_data.csv')
 SENSOR_DATA = get_sensor_data(SENSOR_PATH)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/greenhouse/{grh_num}')
 def get_greenhouse_plot_data(grh_num=1):
